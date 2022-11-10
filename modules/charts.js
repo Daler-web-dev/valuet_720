1260a876cb586fafb6c0943c7e81aaebd2235bbf
Daler <dsarifkulo021@gmail.com>
2022-11-10 12:07:47 +0500

    char function ready

--- modules/charts.js
+++ modules/charts.js
@@ -1,29 +1,27 @@
-const labels = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-];
 
-const data = {
-    labels: labels,
-    datasets: [{
-        label: 'My First dataset',
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: [0, 10, 5, 2, 20, 30, 45],
-    }]
-};
+const createChart = (labels, dataArr, labelText, chartType, canvas, colors) => {
+    const data = {
+        labels: labels,
+        datasets: [{
+            label: labelText,
+            backgroundColor: colors,
+            borderColor: 'transparent',
+            data: dataArr,
+        }]
+    };
+    
+    const config = {
+        type: chartType,
+        data: data,
+        options: {}
+    };
+    
+    const chartOne = new Chart(
+        document.getElementById(canvas),
+        config
+    );
+}
 
-const config = {
-    type: 'line',
-    data: data,
-    options: {}
-};
+createChart(['January','February','March','April','June','May'], [10,20,30,40,50,60], 'chart', 'pie', 'chartOne', ['red', 'blue', 'skyblue'])
+createChart(['January','February','March','April','June','May'], [10,20,30,40,50,60], 'chart two', 'pie', 'chartTwo', ['red', 'blue', 'skyblue'])
 
-const chartOne = new Chart(
-    document.getElementById('chartOne'),
-    config
-);
